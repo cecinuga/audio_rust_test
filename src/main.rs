680f0a83d74f429e763a69fe3a9c6f2974462e43
Marchetti Matteo <=>
2022-11-18 20:25:50 +0100

    go go

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-     println!("Hello, world!");
-}
+use std::{
+    io::BufReader,
+    fs::File,
+};
+use rodio::{
+    OutputStream, Sink, Decoder,
+};
+
+fn main(){ 
+    let (_stream, handler) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&handler).unwrap();
+
+    let file = File::open("./assets_audacity16bit.wav").unwrap();
+    sink.append(Decoder::new(BufReader::new(file)).unwrap());
+
+    sink.sleep_until_end();
+}    
